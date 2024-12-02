1904f1afe87206d203f41dca86e1a19260e7ba1c
GOD-cp <gp21a008@oecu.jp>
2024-12-02 11:41:40 +0900

    Update scriptd.js

--- scriptd.js
+++ scriptd.js
@@ -1,16 +1,4 @@
-// ×ボタンで画像を消す機能の追加
-document.addEventListener("DOMContentLoaded", function() {
-    const closeButtons = document.querySelectorAll('.close-btn',);
 
-    closeButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const img = button.parentElement.querySelector('img'); // ×ボタンの親要素内の画像を取得
-            img.style.display = 'none'; // ×ボタンが押されたら画像を消す
-            button.style.display = 'none'; // ボタン自体も消す
-             
-        });
-        });
-    });
 // ×ボタンで画像を消す機能の追加
 document.addEventListener("DOMContentLoaded", function() {
     const closeButtons = document.querySelectorAll('.close-btn1',);
@@ -34,6 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
             button.style.display = 'none'; // ボタン自体も消す
         });
     });
+});
+
+// 全てのclose-btn11ボタンを取得
+const closeButtons = document.querySelectorAll(".close-btn11");
+
+// 各ボタンにイベントリスナーを追加
+closeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    // 全ての画像要素を削除
+    const images = document.querySelectorAll(".image");
+    images.forEach(image => image.remove());
+  });
 });
 
    
